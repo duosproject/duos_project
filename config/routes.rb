f75e6c8d581dde232d83dfd4c7e255f1abaa1fb2
John <duos.project.luc@gmail.com>
2016-09-22 03:04:24 +0000

    quick update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root 'application#hello'
-end
+
+ root               'static_pages#home'
+ 
+ 
+ get 'publications' => 'static_pages#publications'
+
+ get 'about'     => 'static_pages#about'
+ 
+ get 'contact'   => 'static_pages#contact'
+ 
+ end
